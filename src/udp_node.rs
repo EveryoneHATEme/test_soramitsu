fca889272c5328441dbe73d6e41775bbf7b99beb
EveryoneHATEme <everyonehateme@yandex.ru>
2024-08-01 01:00:23 +0300

    added ctrl c shutdown

--- src/udp_node.rs
+++ src/udp_node.rs
@@ -1,5 +1,6 @@
 use std::net::{SocketAddr, UdpSocket};
 use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
@@ -10,24 +11,27 @@ pub struct Node {
     period: u64,
     known_nodes: Arc<Mutex<Vec<SocketAddr>>>,
     socket: Arc<UdpSocket>,
+    stopped: Arc<AtomicBool>,
 }
 
 
 impl Node {
     pub fn new(port: u16, period: u64) -> Self {
-        let socket = UdpSocket::bind(SocketAddr::from(([127, 0, 0, 1], port))).expect("failed to bind host socket");
-        let known_nodes = Arc::new(Mutex::new(Vec::new()));
+        let socket: Arc<UdpSocket> = Arc::new(UdpSocket::bind(SocketAddr::from(([127, 0, 0, 1], port))).expect("failed to bind host socket"));
+        let known_nodes: Arc<Mutex<Vec<SocketAddr>>> = Arc::new(Mutex::new(Vec::new()));
+        let stopped = Arc::new(AtomicBool::new(false));
 
         Self {
             period, 
             known_nodes,
-            socket: Arc::new(socket),
+            socket,
+            stopped,
         }
     }
     
     pub fn start(&self, connect_to: Option<String>) {
-        let listener_thread = self.run_listener();
-
+        let _listener_thread = self.run_listener();
+        
         if let Some(address) = connect_to {
             let address: SocketAddr = address.parse().unwrap();
             self.known_nodes.lock().unwrap().push(address);
@@ -38,7 +42,20 @@ impl Node {
 
         let sender_thread = self.run_sender();
 
-        listener_thread.join().unwrap();
+        let socket = Arc::clone(&self.socket);
+        let stopped = Arc::clone(&self.stopped);
+        let known_nodes = Arc::clone(&self.known_nodes);
+
+        ctrlc::set_handler(move || {
+            println!("Shutting down...");
+            stopped.store(true, Ordering::SeqCst);
+            let known_nodes = known_nodes.lock().unwrap();
+            for node_address in known_nodes.iter() {
+                socket.send_to(b"stop", node_address).unwrap();
+                println!("Sent stop to {}", node_address);
+            }
+        }).unwrap();
+
         sender_thread.join().unwrap();
     }
 
@@ -46,10 +63,11 @@ impl Node {
         let listener_socket = Arc::clone(&self.socket);
         let socket_copy = Arc::clone(&self.socket);
         let known_nodes = Arc::clone(&self.known_nodes);
+        let stopped = Arc::clone(&self.stopped);
         
         let listener_thread = thread::spawn(move || {
             let mut buffer = [0u8; 1024];
-            loop {
+            while !stopped.load(Ordering::SeqCst) {
                 match listener_socket.recv_from(&mut buffer) {
                     Ok((size, address)) => {
                         let obtained_msg = String::from_utf8_lossy(&buffer[..size]).to_string();
@@ -57,7 +75,7 @@ impl Node {
                         let socket_copy = Arc::clone(&socket_copy);
                         thread::spawn(move || {
                             execute_command(&obtained_msg, address, &known_nodes, &socket_copy);
-                        });
+                        }).join().unwrap();
                     }, 
                     Err(error) => {
                         eprintln!("listener failed: {}", error);
@@ -73,9 +91,10 @@ impl Node {
         let sender_socket = Arc::clone(&self.socket);
         let known_nodes = Arc::clone(&self.known_nodes);
         let period = self.period;
+        let stopped = Arc::clone(&self.stopped);
 
         let sender_thread = thread::spawn(move || {
-            loop {
+            while !stopped.load(Ordering::SeqCst) {
                 thread::sleep(Duration::from_secs(period));
 
                 let mut rng = rand::thread_rng();
@@ -105,6 +124,7 @@ fn execute_command(command: &str, sender: SocketAddr, known_nodes: &Arc<Mutex<Ve
         match command {
             "new" => handle_new_command(sender, known_nodes),
             "list" => handle_list_command(sender, known_nodes, socket),
+            "stop" => handle_stop_command(sender, known_nodes), 
             _ => println!("Unknown command {} from {}", command, sender),
         }
     }
@@ -142,4 +162,13 @@ fn handle_list_response(other_address: &str, known_nodes: &Arc<Mutex<Vec<SocketA
     known_nodes.push(other_address);
 
     socket.send_to(b"new", other_address).unwrap();
+}
+
+
+fn handle_stop_command(sender: SocketAddr, known_nodes: &Arc<Mutex<Vec<SocketAddr>>>) {
+    let mut known_nodes = known_nodes.lock().unwrap();
+
+    known_nodes.retain(|&node_address| node_address != sender);
+
+    println!("{} stopped", sender);
 }
